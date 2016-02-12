4884f90f61f9f09547c2141d933f21a3d00e1de6
abicruz <abi_cruz_12@hotmail.com>
2016-02-11 19:41:24 -0500

    Update RomanNumerals.java

--- RomanNumerals.java
+++ RomanNumerals.java
@@ -13,12 +13,11 @@ public static String IntegerToRoman(int a)
         String romanNum = "";
         int numLet;
         
-        int numbers[]={1000,900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        
-        String letters[]={"M","CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", 
+        String letters[] = {"M","CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", 
             "V", "IV", "I"};
-  
-        for(int x=0; a>0; x++)
+        int numbers[] = {1000,900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        
+        for(int x = 0; a > 0; x++)
         {
             numLet = a/numbers[x];
             for(int i = 1; i <= numLet; i++)
